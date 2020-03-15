6c0e51dfa3acf5213e98fced6e1114ad4628d5cf
Cxb <851145971@qq.com>
2020-03-15 16:25:42 +0800

    require caching

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 
 class Category extends Model
 {
     use SoftDeletes;
+    use Cachable;
     protected $table = 'categories';
     protected $fillable = ['name', 'keywords', 'description', 'sort'];
 }
