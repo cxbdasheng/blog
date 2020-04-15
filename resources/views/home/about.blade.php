3ef3087745e7a8ce069c2891a508db6da4f38bfc
Cxb <851145971@qq.com>
2020-04-15 23:13:11 +0800

    edit about index

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -425,11 +425,26 @@
                 关于我
             </h2>
             <div class="herdimg">
-                <img src="img/about/avatar.jpg" alt="">
+                <img class="lazy" data-original="img/about/head.jpg"  alt="陈大剩头像" title="陈大剩头像" style="opacity: 0.8">
             </div>
             <div class="ab-info">
                 <p>陈大剩 | Cxb </p>
-                <p>PHP全栈工程师 2 years+</p>
+                <p id="year">PHP开发工程师  </p>
+                <script type="application/javascript">
+                    var now_year = new Date().getFullYear();
+                    var now_month = new Date().getMonth() + 1;
+                    var pass_year = now_year - 2017;
+                    if (now_month > 6) {
+                        var pass_months = (now_month - 6) / 12;
+                    } else {
+                        var pass_months = (12 + (now_month - 6)) / 12;
+                        pass_year = pass_year - 1;
+                    }
+                    var pass=pass_year+pass_months;
+                    var dom_year=document.getElementById('year');
+                    var newText=document.createTextNode(pass.toFixed(1)+" years+");
+                    dom_year.appendChild (newText);
+                </script>
                 <p>微信&QQ：851145971</p>
                 <p>邮箱：<a href="mailto:cxb163mail@163.com" target="_blank">cxb163mail@163.com</a></p>
             </div>
@@ -439,7 +454,7 @@
                 微信
             </h2>
             <ul>
-                <img src="img/about/ewm.jpg" style="width: 100%;" alt="二维码">
+                <img class="lazy" data-original="img/about/ewm.jpg"  style="width: 100%;" alt="二维码" title="二维码">
             </ul>
         </div>
     </div>
@@ -448,12 +463,12 @@
             <div class="newsview">
                 <div id="news_infos" class="news_infos">
                     <p>　　Hi，我是陈大剩，一个位90后草根站长，一位户外旅行爱好者，一位持续学习者，一名PHP程序猿^_^。</p>
-                    <img src="img/about/cb.png" style="width: 50%;float: left;margin: 20px;" class="img-fw mb-30" alt="Hi，我是陈大剩" title="Hi，我是陈大剩">
+                    <img class="lazy" data-original="img/about/cb.png" style="width: 50%;float: left;margin: 20px;" class="img-fw mb-30" alt="Hi，我是陈大剩" title="Hi，我是陈大剩">
                     <p>　　大学毕业开始入坑，不知不觉已经从事PHP工作二年了。但是这对门世界上最好的语言的热爱依然不减，因为它是有趣而强大的，我还有很长的路要走！这二年已经习惯了朝7晚1的生活，也因此做了很多的项目，企业官网，商城，小程序，微信开发等等。有一些进步，但是总体提升的速度还是很缓慢，互联网的知识太多，不学习总感觉自己会退步，一定会加倍努力学习！</p>
                     <p>　　做这个博客断断续续花了一年多，从设计到前端展示再到后台都是自己亲力亲为，非常感给我博客提前意见的你们，谢谢！希望从现在开始能建立一个比较完整博客系统，并记录工作上遇到的问题和生活的点滴一起和大家分享，共同进步！</p>
                     <p>　　现在阶段博客还有许多功能未完善，我和把所有的功能都慢慢完善起来，并记录完善日志；与此同时我也会将我的博客整理出来，变成开源项目，供大家研究和测试！</p>
                     <p>　　如果有朋友有问题请教或者发现Bug，可以加我左边的微信好友或者QQ好友，联系我！谢谢。</p>
-                    <p style="float:right;">2018年10月15日15:33:09<img src="img/about/mysignature.png"  style="margin-left: 30px;"  class="mt-30" alt="陈大剩签名" title="陈大剩签名"></p>
+                    <p style="float:right;">2018年10月15日15:33:09<img class="lazy" data-original="img/about/mysignature.png"   style="margin-left: 30px;"  class="mt-30" alt="陈大剩签名" title="陈大剩签名"></p>
                 </div>
                 <div class="clear"></div>
                 <div id="about" class="weixin tvme">
@@ -491,7 +506,7 @@
                                     </div>
                                 </div>
                                 <div class="intro"><p> 作为一个IT码农一天不学习你会不会觉得心里不安？作为一名PHP工程师，我无时无刻在提醒自己要不断学习充实自己，一天不学习我就害怕被这个互联网抛弃了 。读书这么多年，我从来没有像这样努力过。已经很久没玩游戏了，看书和视频都是技术相关的，很久没有看过电视剧了。</p></div>
-                                <div class="img"><img src="img/about/bc.jpg" alt="编程学习" title="编程学习"></div>
+                                <div class="img"><img class="lazy" data-original="img/about/bc.jpg"   alt="编程学习" title="编程学习"></div>
                             </li>
                             <li>
                                 <div class="title">
@@ -500,7 +515,7 @@
                                     </div>
                                 </div>
                                 <div class="intro"><p>生活也是一个不断学习的过程，谁也不可能一出生就什么都会，只有不断的提升自己，增加自己的知识储备，才有更好的发展前景。我有很多爱好，比如游泳、弹琴、溜冰、羽毛球等等，正是因为我这些爱好迫使着我不断学习，从15年开始，我陆续考得了《5级救生员》、《5级游泳教练员》等等。</p></div>
-                                <div class="img"><img src="img/about/yd.jpg" alt="生活学习" title="生活学习"></div>
+                                <div class="img"><img class="lazy" data-original="img/about/yd.jpg"  alt="生活学习" title="生活学习"></div>
                             </li>
                             <li>
                                 <div class="title">
@@ -509,7 +524,7 @@
                                     </div>
                                 </div>
                                 <div class="intro"><p>IT 行业是一个变化非常快的行业，它需要我们持续去学习新的知识和技能。 但是，工作以后，我们经常会发现自己学习的东西很少了，倒不是没有时间去学习， 而是学习的效率太低了。久而久之，就演变成『一年的工作经验，重复用十年』。所以我只能不断努力！加油！</p></div>
-                                <div class="img"><img src="img/about/work.jpg" alt="工作学习" title="工作学习"></div>
+                                <div class="img"><img class="lazy" data-original="img/about/work.jpg"   alt="工作学习" title="工作学习"></div>
                             </li>
                         </ul>
                     </div>
@@ -521,5 +536,6 @@
 @section('js')
     <script>
         document.getElementById("movie").style.height=document.getElementById("movie").scrollWidth*0.6+"px";
+
     </script>
 @endsection
