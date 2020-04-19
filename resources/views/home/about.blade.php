b81a97ba73dc8e485956af467def3446e1735338
Cxb <851145971@qq.com>
2020-04-19 20:19:27 +0800

    edit about

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -229,6 +229,7 @@
                 float: none;
                 width: 100%;
                 height: 100%;
+                display: block;
             }
             .panel{
                 /*width: 100%;*/
