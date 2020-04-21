38787f7bbb642d23d4c838d903d1b4b218743dc3
Cxb <851145971@qq.com>
2020-04-22 00:20:29 +0800

    edit Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('queue:work')->everyMinute();
+        $schedule->command('queue:work --stop-when-empty')->everyMinute();
     }
 
     /**
