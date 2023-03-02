1351a8538b6cf2bff36bdf1b844db6be6d0f6dd9
Dasheng Chen <40051171+cxbdasheng@users.noreply.github.com>
2023-03-02 14:16:45 +0800

    remove genealabs/laravel-model-caching

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
 class BaseModel extends Model
 {
-    use SoftDeletes, Cachable;
+    use SoftDeletes;
 
     /**
      * 为数组 / JSON 序列化准备日期。
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 class User extends Authenticatable
 {
-    use Notifiable,SoftDeletes,Cachable;
+    use Notifiable,SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
